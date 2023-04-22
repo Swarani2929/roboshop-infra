0ab55b1a1da817249941232d4ce1740085ae2a7e
HP <swaranigupthapulluri@gmail.com>
2023-04-22 22:38:50 +0530

    updating ./roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z06113993FP8Y6SPFD2JT"
-  name    = "${var.component}-${var.env}-devops.online"
+  name    = "${var.component}-${var.env}-devops25.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
