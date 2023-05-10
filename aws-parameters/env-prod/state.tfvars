6af14ea2055c204ebab2af9a055a2e7d2e5bc95f
HP <swaranigupthapulluri@gmail.com>
2023-05-10 22:06:33 +0530

    updating ./roboshop-infra

---
+++ aws-parameters/env-prod/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-25"
+key    = "prod/parameters/terraform.tfstate"
+region = "us-east-1"
---
+++ aws-parameters/state.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+resource "aws_ssm_parameter" "foo" {
+  name  = "foo"
+  type  = "String"
+  value = "bar"
+}
+
+variable "parameters" {
+  default = [
+
+  ]
+}
