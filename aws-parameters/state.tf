b5e5e2324917e81c4c524b71c06ee4416305f4e3
HP <swaranigupthapulluri@gmail.com>
2023-05-10 22:12:59 +0530

    updating ./roboshop-infra

--- aws-parameters/state.tf
+++ aws-parameters/state.tf
@@ -4,9 +4,9 @@ terraform {
 
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
-  name  = "foo"
-  type  = "String"
-  value = "bar"
+  name  = "var.parameters[count.index].name"
+  type  = "var.parameters[count.index].type"
+  value = "var.parameters[count.index].value"
 }
 
 variable "parameters" {}
